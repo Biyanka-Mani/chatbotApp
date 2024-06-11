09854ad7f5b05289383f25ac790a25c3998b16ff
Biyanka-Mani <biyanka167@gmail.com>
2024-06-11 13:15:08 +0530

    Added Authetication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,20 @@ class SessionsController < ApplicationController
     def new
     end
     def create
+        user =User.find_by(username: params[:session][:username])
+        if user&&user.authenticate(params[:session][:password])
+            session[:user_id]=user.id
+            flash[:success]="Login Successfully!"
+            redirect_to root_path
+        else
+            flash.now[:error]="Login Failed!"
+            render :new,status: :unprocessable_entity
+        end
     end
+
     def destroy
+        session[:user_id]=nil
+        flash[:success]="Logout Successfully!"
+        redirect_to login_path
     end
 end
