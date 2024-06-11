f29818e79c703baf857905c3ffc6c2faf489474c
Biyanka-Mani <biyanka167@gmail.com>
2024-06-11 10:20:52 +0530

    Naviagtion links added at navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root 'chatroom#index'
   get '/login', to: "sessions#new"
+   
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
